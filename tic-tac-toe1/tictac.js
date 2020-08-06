4758c7d098a2ac2df0227681fa4edb1a121d95fc
rohit <rohit.sambyal1740@gmail.com>
2020-08-07 03:14:04 +0530

    Updated Web

--- tic-tac-toe1/tictac.js
+++ tic-tac-toe1/tictac.js
@@ -5,6 +5,9 @@ var tictac = (function(){
 	var rowCount = "3";
 	var colCount = "3";
 	var streakCount = "3";
+	var turn=0;
+	var ids=[];
+	var a=0;
 
     function updateMatrix(){
         initialState = [];
@@ -23,12 +26,57 @@ var tictac = (function(){
 	    streakCount = document.getElementById("streak-input").value;
 	    updateMatrix();
 	}
-
-    function onCellClickHandler(element){
-        console.log("cell clicked", event);
+	function displayMarkX(element){
         let r = element.getAttribute("data-row");
         let c = element.getAttribute("data-column");
-        initialState[r][c] = 0
+	    var comm=`<div class="column" data-row="${r}" data-column="${c}">X</div>`;
+	    var idForElement=`rowcol${r}${c}`;
+	    var dataOfInnerContent=document.getElementById(idForElement).textContent;
+	    console.log(dataOfInnerContent);
+	    if (dataOfInnerContent == 'O'|| dataOfInnerContent == 'X'){
+	        console.log("Invalid turn");
+	        return false; }
+	    else{
+	        document.getElementById(idForElement).innerHTML=comm;
+	        return true;}
+	}
+
+	function displayMarkO(element){
+	    let r = element.getAttribute("data-row");
+        let c = element.getAttribute("data-column");
+	    var comm=`<div class="column" data-row="${r}" data-column="${c}">O</div>`;
+	    var idForElement=`rowcol${r}${c}`;
+	    var dataOfInnerContent=document.getElementById(idForElement).textContent;
+	    console.log(dataOfInnerContent);
+	    if (dataOfInnerContent == 'O'|| dataOfInnerContent == 'X'){
+	        console.log("Invalid turn");
+	        return false; }
+	    else{
+	        document.getElementById(idForElement).innerHTML=comm;
+	        return true;}
+	}
+
+    function onCellClickHandler(element){
+
+        let r = element.getAttribute("data-row");
+        let c = element.getAttribute("data-column");
+        if(turn%2!=0)
+        {
+        var bol=displayMarkX(element);
+        if (bol==false){
+        turn=turn-1;
+        }
+        initialState[r][c] = 0;
+        }
+        else
+        {
+        var bol=displayMarkO(element);
+        if (bol==false){
+        turn=turn-1;
+        }
+        initialState[r][c] =1;
+        }
+        turn=turn+1;
     }
 
 	function renderUI(elementId){
@@ -46,7 +94,7 @@ var tictac = (function(){
 		for (let i=0; i<numRow; i++){
 			var row = `<div class="row">`
 			for (let j=0; j<numCol; j++){
-				var column = `<div class="column" data-row=${i} onclick="tictac.onCellClickHandler(this)" data-column=${j}>${initialState[i][j]}</div>`
+				var column = `<div class="column" id="rowcol${i}${j}" data-row=${i} onclick="tictac.onCellClickHandler(this)" data-column=${j}>${initialState[i][j]}</div>`
 				row += column
 			}
 			row += '</div>'
